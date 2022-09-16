acaadedcf0e95f21874d3c951a0b94868240dd05
Glorine Vihendah <92040630+gvihendah@users.noreply.github.com>
2022-09-17 00:37:15 +0300

    Update 8-print_square.c
    fixed bug

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 
 /**
  * print_square - Print a square with #.
@@ -8,23 +8,23 @@ nclude "main.h"
  */
 void print_square(int size)
 {
-	int i;
-	int j;
-	i = 0;
-	if (size <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		while (i < size)
-		{
-			for (j = 0; j < size; j++)
-			{
-				_putchar('#');
-			}
-			i++;
-			_putchar('\n');
-		}
-	}
+int i;
+int j;
+i = 0;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+while (i < size)
+{
+for (j = 0; j < size; j++)
+{
+_putchar('#');
+}
+i++;
+_putchar('\n');
+}
+}
 }
